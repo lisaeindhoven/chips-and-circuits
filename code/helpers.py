f419db208668da4bdfc8917739c138b3069cc9de
sargos-coder <mik.schutte1998@gmail.com>
2020-06-10 16:04:39 +0200

    Manual revert to commit 3dfa0aa2544f1470f49332548669e4d505e1ee07

---
+++ code/helpers.py
@@ -0,0 +1,39 @@
+"""
+helpers.py
+
+Minor Programmeren, Programmeertheorie, Chips & Circuits
+Misbaksels: Mik Schutte, Sebastiaan van der Laan & Lisa Eindhoven
+
+This file contains the helper function for main.py
+"""
+import csv
+
+from models.gates import *
+from models.grid import *
+from models.nets import *
+
+def get_gates_and_nets(gate_coordinates_csv_path, gate_connections_csv_path):
+    """returns a dictionary with the gate number, coordinate and connected gates"""
+    # Get coordinates
+    with open(gate_coordinates_csv_path, mode='r') as f:
+        reader = csv.reader(f)
+        gates = []
+        for row in reader:
+            if row[0] != "chip":
+                gate = Gate(row[0], row[1], row[2])
+                gates.append(gate)
+
+    # Get connections
+    with open(gate_connections_csv_path, mode='r') as f:
+        reader = csv.reader(f)
+        count = 1
+        nets = []
+        for row in reader:
+            if row[0] != "chip_a":
+                net = Nets(count, gates[int(row[0])-1], gates[int(row[1])-1])
+                gates[int(row[0])-1].add_net(net)
+                gates[int(row[1])-1].add_net(net)
+                nets.append(net)
+                count += 1
+
+    return gates, nets
---
+++ code/save_results.py
@@ -0,0 +1,48 @@
+import csv
+import numpy as np
+from datetime import datetime
+
+def get_results(save_folder, chip_name, nets, grid):
+    """"Save the results in a csv file in the given folder,
+    as a file called output with the date and time.
+    Returns a string with the total costs and conflicts.
+    Conflicts are faults in the results, like more a collision or two wires on one path. """
+
+    wire_count = 0
+    intersection_count = 0
+    conflict_count = 0
+
+    # Check nets and add wires
+    for net in nets:
+        wire_count += net.wire_count()
+
+    # Check grid matrix for intersections           
+    for index, coordinate_value in np.ndenumerate(grid.matrix):
+        if coordinate_value != []:
+            continue
+
+        if len(coordinate_value) == 2:
+            intersection_count += 1
+        # TODO: deze weghalen bij het officieel inleveren, dit is alleen voor onze check dat we zien dat er iets fout gaat
+        elif len(coordinate_value) > 2:
+            conflict_count += 1
+        # TODO: misschien nog testen op dat geen verbinding twee keer wordt gebruikt
+
+    # Count total costs
+    total_costs = wire_count + 300 * intersection_count
+    
+    # Create folder name with current date and time
+    now = datetime.now()
+    date_time = now.strftime("%m.%d.%Y_%H.%M.%S")
+    result_doc = save_folder + "output_" + date_time + ".csv"
+
+    # Create new csv file and write it
+    with open(result_doc, mode='w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["net", "wires"])
+        # TODO: coordinaten worden nu met spaties gegeven, check50 vind dit niet okay
+        for net in nets:
+            writer.writerow([net.get_connection(), net.wires])
+        writer.writerow([chip_name, total_costs])
+
+    return f"Total costs: {total_costs}. Total conflicts: {conflict_count}"
