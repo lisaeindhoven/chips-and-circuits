65ebf549e4f87dd13dcb4ab9c483f34b811d63ee
Sargos-coder <mik.schutte1998@gmail.com>
2020-06-11 16:51:18 +0200

    Fully implemented visualiser.py, now showing correct sizes.

--- code/visualisation/visualiser.py
+++ code/visualisation/visualiser.py
@@ -11,24 +11,25 @@ def visualiser(grid, gates, paths):
     # Plot 3D axes
     fig = plt.figure(figsize=(x_dim, z_dim))
     ax = mplot3d.Axes3D(fig)
-    ax.set_zlim(0, z_dim)
+    ax.set_zlim(0, z_dim*0.5)
     plt.axis('off')
-
-    # Create and plot grid structure
-    x = np.arange(0, x_dim, 1)
-    y = np.arange(0, y_dim, 1)
-    xx, yy = np.meshgrid(x, y, sparse=False)
+    
+    #Create and plot grid structure
+    x = np.arange(0, x_dim+1, 1)
+    y = np.arange(0, y_dim+2, 1)
+    xx, yy = np.meshgrid(x, y)
     for z in np.arange(start=0, stop=z_dim):
         plt.plot(xx, yy, z, 'gray')
         plt.plot(yy, xx, z, 'gray')
 
     # Place gates
     x_scat, y_scat, z_scat = [[], [], []]
+
     for gate in gates:
         x_scat.append(gate.x)
         y_scat.append(gate.y)
         z_scat.append(gate.z)
-    ax.scatter3D(x_scat, y_scat, z_scat, s=100, marker='s', c='red', alpha=1)
+    ax.scatter3D(x_scat, y_scat, z_scat, s=100, marker='s', c='red')
 
     # Place paths
     for path in paths:
@@ -36,10 +37,4 @@ def visualiser(grid, gates, paths):
         y_val = [xy[1] for xy in path]
         plt.plot(x_val, y_val, 'blue')
 
-    # # Rotate axis and update
-    # for angle in range(0, 360):
-    #     ax.view_init(30, angle)
-    #     plt.draw()
-    #     plt.pause(0.05)
-    #     ax.view_init(20, 30)
-    plt.show()
+    plt.show()    
