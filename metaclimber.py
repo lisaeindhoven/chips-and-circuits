779905b23ec957c18620ce6cb270748a90f3701c
svdl2020 <sseebbaass@gmail.com>
2020-06-19 15:53:04 +0200

    added metaclimber.py to oversee our methods

---
+++ metaclimber.py
@@ -0,0 +1,44 @@
+import copy
+# METACLIMBER WORDT AANGESTUURD VANUIT MAIN
+# MAIN HEEFT DE INTERFACE EN ROEPT METACLIMBER AAN MET BEPAALDE INPUT
+
+# PSEUDOCODE:
+# 1 INPUT LADEN EN LEGE DINGEN GENEREREN
+# 2 EERSTE RUN MET ? CONSTRAINTS
+# 3 PADEN VERWIJDEREN DIE ?
+# 4 PADEN HERLEGGEN MET ? CONSTRAINTS
+# ????
+# 99 LOOP: 
+# VERWIJDER DUURSTE PAD
+# LEG OVERGEBLEVEN PADEN
+
+Class Metaclimber:
+    """ 
+    The Metaclimber class creates chip circuitry in multiple steps 
+    by generating suboptimal results and gradually improving upon them.
+    """
+    # load input data and keep track of best value
+    def __init__(chip, netlist, method):
+        self.grid, self.nets = 
+        self.best_value = 
+
+    # main function om stapsgewijs te runnen en descenden
+    def execute():
+    """ executes metaclimber x number of times or until there's no more improvement
+    """
+        # PSEUDOCODE:
+        # 1 INPUT LADEN EN LEGE DINGEN GENEREREN
+        # 2 EERSTE RUN MET ? CONSTRAINTS
+        # 3 PADEN VERWIJDEREN DIE ?
+        # 4 PADEN HERLEGGEN MET ? CONSTRAINTS
+        # ????
+        # 99 LOOP: 
+        # VERWIJDER DUURSTE PAD
+        # LEG OVERGEBLEVEN PADEN
+
+    # lay (all) remaining paths with certain constraints
+    def run(algorithm/constraints):
+
+    # select criterium to erase path
+    def hilldescent(solution, eraser):
+
